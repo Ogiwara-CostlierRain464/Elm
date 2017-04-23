7a705c174ec3f432983f914afdfea317187d1e37
ogiwara <yushiogiwara@gmail.com>
2017-04-23 11:26:19 +0900

    ~> Means Single.map

--- Q1.elm
+++ Q1.elm
@@ -1,3 +1,50 @@
-import Html exposing (Html,text)
+module App exposing (..)
 
-main = text "w"
+import Html exposing(Html,div,text,program)
+
+{-Model-}
+
+type alias Model =
+    String
+
+init : (Model,Cmd Msg)
+init = 
+    ("Hello",Cmd.none)
+
+--Messages
+
+type Msg
+  = NoOp
+
+--VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [text model]
+
+--UPDATE
+
+update: Msg -> Model -> (Model,Cmd Msg)
+update msg model =
+    case msg of
+        NoOp ->
+            (model,Cmd.none)
+
+--SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+--Main
+
+main : Program Never Model Msg
+main =
+    program
+        {
+            init = init,
+            view = view,
+            update = update,
+            subscriptions = subscriptions
+        }
---
+++ markdown1.elm
@@ -0,0 +1,17 @@
+import Markdown
+
+main = 
+    Markdown.toHtml [] markdown
+
+
+markdown = """
+
+[Markdown](http://daringfireball.net/projects/markdown/)
+Elmは
+    *FRP
+    *関数型
+    *ムズイ
+
+マークダウンの試験中
+`Element`  
+"""
